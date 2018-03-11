2ec9b30f7565ecf438d82f2766200a6a29c1c39d
rover <mswxhdh@gmail.com>
2018-03-11 17:21:27 +0800

    init commit

---
+++ Assets/Scripts/UNETClient.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UNETClient : MonoBehaviour {
+
+    //    public InputField IpInput;
+    //    public InputField PortInput;
+    //    private int port;
+    //    public InputField MsgInput;
+    //    public Text MsgRevText;
+    //
+    //    public Text Log;
+
+    public GameObject Mario;
+    public GameObject ArImage;
+    private float _speed = 1.0f;
+    private float _scale = 1.0f;
+
+//    public Image[] GestImages;
+
+    private string LogString { get; set; }
+    private UnetClientBase UnetClientBase;
+
+
+    // Use this for initialization
+    void Start () {
+        UnetClientBase = gameObject.GetComponent<UnetClientBase>();
+
+        UnetClientBase.ConnectionEvent += UnetClientBase_ConnectionEvent;
+        UnetClientBase.DisconnectionEvent += UnetClientBase_DisconnectionEvent;
+        UnetClientBase.DataEvent += UnetClientBase_DataEvent;
+
+        StartCoroutine(connectToServer());
+    }
+
+    private IEnumerator connectToServer()
+    {
+        yield return new WaitForSeconds(5f);
+        Debug.Log("start Connect");
+        ConnectedToServer();
+    }
+
+    private void UnetClientBase_DataEvent(object sender, UnetClientBase.UnetDataMsg e)
+    {
+        
+        if (string.IsNullOrEmpty(e.Msg)) return;
+//        if (e.Msg.Equals("0"))
+//        {
+//            Debug.Log($"0");
+//        }
+//        else if (e.Msg.Equals("w"))
+//        {
+//            Debug.Log($"w");
+//        }
+//        else if (e.Msg.Equals("s"))
+//        {
+//            Debug.Log($"s");
+//        }
+//        else
+//        {
+//            Debug.Log($"default");
+//        }
+        int commandNum;
+        int.TryParse(e.Msg, out commandNum);
+        switch (commandNum)
+        {
+            case 0:
+                Mario.transform.parent = Mario.transform.parent == null ? ArImage.transform:null; 
+                break;
+            case 1:
+                Mario.transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * _speed);
+                break;
+            case 2:
+                Mario.transform.Translate(new Vector3(0, 0, -1) * Time.deltaTime * _speed);
+                break;
+            case 3:
+                Mario.transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * _speed);
+                break;
+            case 4:
+                Mario.transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * _speed);
+                break;
+            case 5:
+//                float.TryParse(e.Msg, out _scale);
+                Mario.transform.localScale = Mario.transform.localScale * 1.1f;
+                break;
+            case 6:
+//                float.TryParse(e.Msg, out _scale);
+                Mario.transform.localScale = Mario.transform.localScale * 0.9f;
+                break;
+            default:
+                float.TryParse(e.Msg, out _speed);
+                break;
+        }
+    }
+
+    private void UnetClientBase_DisconnectionEvent(object sender, UnetClientBase.UnetConnectionMsg e)
+    {
+//        LogString = $"Server {e.ConnectionId} Disconnect!";
+    }
+
+    private void UnetClientBase_ConnectionEvent(object sender, UnetClientBase.UnetConnectionMsg e)
+    {
+//        LogString = $"Server {e.ConnectionId} Connect!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ConnectedToServer()
+    {
+//        UnetClientBase.ConnectToServer(IpInput.text,port);
+        UnetClientBase.ConnectToServer("192.168.0.146",9696);
+    }
+
+    public void DisConnectedToServer()
+    {
+        UnetClientBase.DisconnectToServer();
+    }
+
+    public void SendMessageToServer()
+    {
+//        UnetClientBase.SendMessageToServer(MsgInput.text);
+    }
+
+//    private string GetIp()
+//    {
+//        string name = Dns.GetHostName();
+//        IPAddress[] ipadrlist = Dns.GetHostAddresses(name);
+//        foreach (IPAddress ipa in ipadrlist)
+//        {
+//            if (ipa.AddressFamily == AddressFamily.InterNetwork)
+//            {
+//                return ipa.ToString();
+//            }
+//        }
+//        return "";
+//    }
+}
---
+++ Assets/Scripts/UnetClientBase.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class UnetClientBase : MonoBehaviour
+{
+    private int hostId;
+    private int recHostId;
+    private int connectionId;
+    private int myConnectionId;
+    private int channelId;
+    private int myReliableChannelId;
+    private byte[] recBuffer = new byte[1024];
+    private int bufferSize = 1024;
+    private int dataSize;
+    private byte error;
+    // Use this for initialization
+
+    public event EventHandler<UnetConnectionMsg> ConnectionEvent;
+    public event EventHandler<UnetConnectionMsg> DisconnectionEvent;
+    public event EventHandler<UnetDataMsg> DataEvent;
+
+    void Start () {
+	    NetworkTransport.Init();
+    }
+
+    public class UnetDataMsg : EventArgs
+    {
+        public int HostId;
+        public int ConnectionId;
+        public int ChannelId;
+        public string Msg;
+
+        public UnetDataMsg(int hostId, int connectionId, int channelId, string msg)
+        {
+            HostId = hostId;
+            ConnectionId = connectionId;
+            ChannelId = channelId;
+            Msg = msg;
+        }
+    }
+    public class UnetConnectionMsg : EventArgs
+    {
+        public int HostId;
+        public int ConnectionId;
+        public int ChannelId;
+        public string ClientIp;
+        public int ClientPort;
+
+        public UnetConnectionMsg(int hostId, int connectionId, int channelId, string clientIp, int clientPort)
+        {
+            HostId = hostId;
+            ConnectionId = connectionId;
+            ChannelId = channelId;
+            ClientIp = clientIp;
+            ClientPort = clientPort;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        recBuffer = new byte[1024];
+        NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+        switch (recData)
+        {
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.ConnectEvent:
+                ConnectionEvent?.Invoke(null,
+                    new UnetConnectionMsg(recHostId, connectionId, channelId,"", 0));
+                break;
+            case NetworkEventType.DataEvent:
+                DataEvent?.Invoke(null, new UnetDataMsg(hostId, connectionId, channelId, System.Text.Encoding.UTF8.GetString(recBuffer)));
+                break;
+            case NetworkEventType.DisconnectEvent:
+                DisconnectionEvent?.Invoke(null,
+                    new UnetConnectionMsg(recHostId, connectionId, channelId, "", 0));
+                break;
+        }
+    }
+
+    public int ConnectToServer(string ip, int port)
+    {
+        ConnectionConfig connectionConfig = new ConnectionConfig();
+        myReliableChannelId = connectionConfig.AddChannel(QosType.Reliable);
+        HostTopology hostTopology = new HostTopology(connectionConfig, 2);
+        hostId = NetworkTransport.AddHost(hostTopology);
+        return myConnectionId = NetworkTransport.Connect(hostId, ip, port, 0, out error);
+    }
+
+    public byte DisconnectToServer()
+    {
+        NetworkTransport.Disconnect(hostId, myConnectionId, out error);
+        return error;
+    }
+
+    public byte SendMessageToServer(string msg)
+    {
+        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(msg);
+        int size = buffer.Length;
+        NetworkTransport.Send(hostId, myConnectionId, myReliableChannelId, buffer, size, out error);
+        return error;
+    }
+
+
+}
