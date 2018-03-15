ce16b1ad9caf8de63abf3abb3b227629d78b8449
HaoDehong <mswxhdh@126.com>
2018-03-15 23:07:30 +0800

    complete 2D part

---
+++ Assets/Scripts/MarioController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioController : MonoBehaviour
+{
+
+    /// <summary>
+    /// UNET Settings
+    /// </summary>
+    public string Ip;
+    public int Port;
+    private UnetClientBase unetClientBase;
+
+    /// <summary>
+    /// Mario Controll
+    /// </summary>
+    public GameObject Mario;
+
+    public GameObject 
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
